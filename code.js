da900d07deb40048d71bc6d2f0af4ce546f455d9
Reza Shirazi <itreza7@gmail.com>
2022-03-18 16:06:39 +0330

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
 var interval_aquarium = setInterval(function () {
-    $('.fishGame_box_aquarium').find('img[src$="https://www.rtl-theme.com/wp-content/themes/rtl-theme/events/Nowruz/dist/Active/images/Group 8710.svg"]').click();
+    $('.fishGame__box__aquarium').find('img[src$="https://www.rtl-theme.com/wp-content/themes/rtl-theme/events/Nowruz/dist/Active/images/Group 8710.svg"]').click();
 },1000);
